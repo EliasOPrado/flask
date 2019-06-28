9bd1aaac7e5a751bebfd008b35d761cec9fccb91
Oliveira <eliaspradoprofessional@outlook.com>
2019-06-28 19:33:41 +0000

    embedding json into the about b

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,9 +9,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
     
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/races.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", races=data)
 
 @app.route("/contact")
 def contact():
